bc138c794c641c7950a3654983b503212ce3c3eb
GuceAlkaTruse <60811855+GuceAlkaTruse@users.noreply.github.com>
2020-03-27 13:27:58 +0200

    first commit

---
+++ Assets/Scripts/Signaling.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Signaling : MonoBehaviour
+{
+    private AudioSource _source;
+
+    private void Start()
+    {
+        _source = GetComponent<AudioSource>();
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _source.Play();
+            StartCoroutine(VolumeIncrement());
+        }
+    }
+
+    private IEnumerator VolumeIncrement()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            _source.volume += 0.1f;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
